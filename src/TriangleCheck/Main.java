e1d1f4bbb4c9dca37f1c4f4b068769406de64c0f
Svetoslav Mateev <svetoslav.mateev@gmail.com>
2023-08-24 17:27:55 +0300

    New changes

--- src/TriangleCheck/Main.java
+++ src/TriangleCheck/Main.java
@@ -11,19 +11,32 @@ public class Main {
         float angle2 = input.nextFloat();
         System.out.println("Enter value for the third angle in degree:");
         float angle3 = input.nextFloat();
+        input.close();
+
 
         if ((angle1 + angle2 + angle3) == 180) {
             String positiveStart = "It is possible to create";
             String answerEnd = "triangle with this values for the angles.";
             if (angle1 < 90 && angle2 < 90 && angle3 < 90) {
-                System.out.println(positiveStart + " an acute " + answerEnd);
+                System.out.println(positiveStart + triangleType(angle1, angle2, angle3) + " acute " + answerEnd);
             } else if (angle1 == 90 || angle2 == 90 || angle3 == 90) {
-                System.out.println(positiveStart + " a right angled " + answerEnd);
+                System.out.println(positiveStart + triangleType(angle1, angle2, angle3) + " right angled " + answerEnd);
             } else if (angle1 > 90 || angle2 > 90 || angle3 > 90) {
-                System.out.println(positiveStart + " an obtuse " + answerEnd);
+                System.out.println(positiveStart + triangleType(angle1, angle2, angle3) + " obtuse " + answerEnd);
             }
         } else {
             System.out.println("It is not possible to create a triangle with this values for the angles.");
         }
+    }
+
+    public static String triangleType(float ang1, float ang2, float ang3) {
+        if (ang1 == 60) {
+            return " an equilateral";
+        } else if (ang1 == ang2 || ang1 == ang3 || ang2 == ang3) {
+            return " an isosceles";
+        } else {
+            return " a multifaceted";
+        }
+
     }
 }
