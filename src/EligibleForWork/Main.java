94d3512e970c77fb480130f06675bd0a63357164
Svetoslav Mateev <svetoslav.mateev@gmail.com>
2023-08-28 17:20:08 +0300

    New changes

--- src/EligibleForWork/Main.java
+++ src/EligibleForWork/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Input your age:");
+        System.out.print("Input your age: ");
         byte age = input.nextByte();
         if (age >= 16){
             System.out.println("You are eligible to work.");
