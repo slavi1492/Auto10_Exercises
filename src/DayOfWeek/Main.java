21e2fc3bc52bdcf630fa9f254f50109533520bef
Svetoslav Mateev <svetoslav.mateev@gmail.com>
2023-08-28 17:43:23 +0300

    New changes

--- src/DayOfWeek/Main.java
+++ src/DayOfWeek/Main.java
@@ -3,11 +3,18 @@ package DayOfWeek;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Input number between 1-7:");
-        int dayOfWeek = input.nextInt();
-        switch (dayOfWeek){
+
+
+    public static void main(String[] args) {
+        int dayOfWeek;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Input number between 1-7:");
+            dayOfWeek = input.nextInt();
+        }
+        while (dayOfWeek > 7 || dayOfWeek < 1);
+
+        switch (dayOfWeek) {
             case 1:
                 System.out.println("It is " + dayOfWeek + "-st day of the week.");
                 break;
