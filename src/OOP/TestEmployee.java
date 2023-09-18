81471ac8fc13f9b035038c9091c0b1c2cb98f23d
Svetoslav Mateev <svetoslav.mateev@gmail.com>
2023-09-18 16:55:32 +0300

    Changes

---
+++ src/OOP/TestEmployee.java
@@ -0,0 +1,11 @@
+package OOP;
+
+public class TestEmployee {
+    public static void main(String[] args) {
+        Employee worker1 = new Employee(0001, "Pesho", "Peshev", 1000.12);
+        System.out.println(worker1.getName());
+        System.out.println(worker1.getAnnualSalary());
+        System.out.println(worker1.raisSalary(15));
+        worker1.employeeToString();
+    }
+}
