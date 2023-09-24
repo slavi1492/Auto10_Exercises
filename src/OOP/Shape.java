e18011e1c325f5775ee6daf4af0ab8dccd3be4f2
Svetoslav Mateev <svetoslav.mateev@gmail.com>
2023-09-24 15:00:56 +0300

    Changes

--- src/OOP/Shape.java
+++
@@ -1,9 +0,0 @@
-package OOP;
-
-public abstract class Shape {
-    public abstract double getArea(double r);
-
-    public abstract double getPerimeter(double r);
-
-    public abstract String printShape();
-}
